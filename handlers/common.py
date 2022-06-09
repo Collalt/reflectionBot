85895be0873c717d8863da803e32599bb6b7d66b
Collalt <collalt@yandex.ru>
2022-06-10 00:36:51 +1000

    created registration placeholder

--- handlers/common.py
+++ handlers/common.py
@@ -3,11 +3,13 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State
 from start_bot import bot, dp
 from emoji import emojize
+from states.situation import Goal
 
 
 async def start_command_handler(message: types.message):
     # TODO check if user exist in DataBase
-    await message.answer(emojize('WelCum :waving_hand:'))
+    await message.answer(emojize('WelCum :waving_hand: \n Set up your goal'))
+    await Goal.waiting_for_goal.set()
 
 
 async def help_command_handler(message: types.message):
