384a696a96bcd5bfa3aca5e25f3f82f5c6eae2fc
Collalt <collalt@yandex.ru>
2022-06-12 01:23:33 +1000

    remove many ; bruh

--- model/model.py
+++ model/model.py
@@ -1,8 +1,8 @@
-from msilib.schema import Error
 from os import getenv
 from pymongo import MongoClient
 
-db_uri = 'mongodb+srv://{}:{}@cluster0.2ugr3.mongodb.net/?retryWrites=true&w=majority'.format(getenv('DB_LOGIN'), getenv('DB_PASSWORD'))
+db_uri = 'mongodb+srv://{}:{}@cluster0.2ugr3.mongodb.net/?retryWrites=true&w=majority'.format(getenv('DB_LOGIN'),
+                                                                                              getenv('DB_PASSWORD'))
 client = MongoClient(db_uri)
 
 db = client["database"]
@@ -10,29 +10,31 @@ db = client["database"]
 customers = db["users"]
 users = db['users']
 
-async def get_user(user_id):
-  if (user_id is None):
-    raise ValueError("user_id is not provided")
 
-  return users.find_one({"_id": user_id});
+async def get_user(user_id):
+    if user_id is None:
+        raise ValueError("user_id is not provided")
+    return users.find_one({"_id": user_id})
+
 
 async def add_user(user_id):
-  if (user_id is None):
-    raise ValueError("user_id is not provided")
+    if user_id is None:
+        raise ValueError("user_id is not provided")
 
-  return users.insert_one({"_id": user_id})
+    return users.insert_one({"_id": user_id})
 
-async def edit_user(user_id, goal = None, term = None):
-  if (user_id is None):
-    raise ValueError("user_id is not provided")
 
-  user = users.find_one({"_id": user_id})
+async def edit_user(user_id, goal=None, term=None):
+    if user_id is None:
+        raise ValueError("user_id is not provided")
 
-  if (user is not None):
-    newData = {}
-    newData.goal = goal or user.goal
-    newData.term = term or user.term
+    user = users.find_one({"_id": user_id})
 
-    return users.update_one({"_id": user_id}, {"$set": newData})
-  else:
-    raise ValueError("User with provided id not found");
+    if user is not None:
+        newData = {}
+        newData.goal = goal or user.goal
+        newData.term = term or user.term
+
+        return users.update_one({"_id": user_id}, {"$set": newData})
+    else:
+        raise ValueError("User with provided id not found")
